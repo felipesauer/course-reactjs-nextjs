80f5a0553911f98abb8a835e797ca985ebf26ce3
felipesauer <felipesauer11@hotmail.com>
2023-07-19 17:59:38 -0400

    - commit project-2

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,9 +1,26 @@
+import P from 'prop-types';
+import { useState } from 'react';
 import './App.css';
 
+const Button = ({ incrementButton }) => {
+  return <button onClick={() => incrementButton(10)}>+</button>;
+};
+
+Button.propTypes = {
+  incrementButton: P.func,
+};
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = (num) => {
+    setCounter(counter + num);
+  };
+
   return (
     <div className="App">
-      <h1>Oi</h1>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }

66e38c2fce908ef26b92213cbd5e6d3f917fb7be
felipesauer <felipesauer11@hotmail.com>
2023-07-21 14:25:42 -0400

    - commit project-2

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,12 +1,44 @@
+import { useReducer } from 'react';
+import { globalState } from './contexts/AppContext/data';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  // eslint-disable-next-line no-unused-vars
+  const { counter, title, body } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleDateString('pt-BR'),
+          })
+        }
+      >
+        muda
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>inverter</button>
+    </div>
   );
 }
 

53a074b99d00def8fa35e11648cf20fa34fab8e4
felipesauer <felipesauer11@hotmail.com>
2023-07-21 17:43:36 -0400

    - commit project-2

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,43 +1,9 @@
-import { useReducer } from 'react';
-import { globalState } from './contexts/AppContext/data';
 import './App.css';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'muda': {
-      console.log('Chamou muda', action.payload);
-      return { ...state, title: action.payload };
-    }
-    case 'inverter': {
-      console.log('Chamou inverter');
-      const { title } = state;
-      return { ...state, title: title.split('').reverse().join('') };
-    }
-  }
-  return { ...state };
-};
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, globalState);
-  // eslint-disable-next-line no-unused-vars
-  const { counter, title, body } = state;
-
   return (
     <div>
-      <h1>
-        {title} {counter}
-      </h1>
-      <button
-        onClick={() =>
-          dispatch({
-            type: 'muda',
-            payload: new Date().toLocaleDateString('pt-BR'),
-          })
-        }
-      >
-        muda
-      </button>
-      <button onClick={() => dispatch({ type: 'inverter' })}>inverter</button>
+      <h1>Oi</h1>
     </div>
   );
 }
--- project-2/src/components/Div/index.jsx
+++
@@ -1,9 +0,0 @@
-import { H1 } from '../H1';
-import { P } from '../P';
-
-export const Div = () => (
-  <>
-    <H1 />
-    <P />
-  </>
-);

3c0f3cc1638294b3f90cb9c702e2dda457cad908
felipesauer <felipesauer11@hotmail.com>
2023-08-07 21:34:01 -0300

    - commit project-3

--- projects/3/src/templates/Home/index.jsx
+++ projects/3/src/templates/Home/index.jsx
@@ -13,12 +13,12 @@ const useAsync = (asyncFunction, shouldRun) => {
 
     return asyncFunction()
       .then((response) => {
-        setStatus('settled');
         setResult(response);
+        setStatus('settled');
       })
       .catch((error) => {
-        setStatus('error');
         setError(error);
+        setStatus('error');
       });
   }, [asyncFunction]);
 
@@ -30,6 +30,7 @@ const useAsync = (asyncFunction, shouldRun) => {
 };
 
 const fetchData = async () => {
+  await new Promise((r) => setTimeout(r, 2000));
   const data = await fetch('https://jsonplaceholder.typicode.com/posts');
   const json = await data.json();
   return json;
@@ -38,5 +39,19 @@ const fetchData = async () => {
 export const Home = () => {
   const [reFetchData, result, error, status] = useAsync(fetchData, true);
 
-  return <pre>{result && JSON.stringify(result, null, 2)}</pre>;
+  if (status === 'idle') {
+    return <pre>Nada executando</pre>;
+  }
+
+  if (status === 'pending') {
+    return <pre>Loading...</pre>;
+  }
+
+  if (status === 'error') {
+    return <pre>{error.message}</pre>;
+  }
+
+  if (status === 'settled') {
+    return <pre>{result && JSON.stringify(result, null, 2)}</pre>;
+  }
 };
